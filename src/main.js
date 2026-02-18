816e8387328d35133c72df2b890d5a1579690798
melshabazz2 <melshabazz2@gmail.com>
2026-02-18 15:03:34 -0500

    completed render collection on page load

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { getShows } from "./fetch-helpers.js";
 import { getSingleShow } from "./fetch-helpers.js";
 import { init } from "./app.js";
 
+const contactForm = document.querySelector('#search-form')
+contactForm.addEventListener('click', (event) => {
+    event.preventDefault()
+
+})
+
 getShows();
 getSingleShow();
 init();
